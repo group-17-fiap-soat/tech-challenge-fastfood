6b81608f038c7a171a1a8a40b673cb7b259af623
idd_fmarques <idd_fmarques@uolinc.com>
2024-11-07 23:57:46 -0300

    refactor: health check status upgrade

--- src/main/kotlin/tech/challenge/fastfood/fastfood/adapters/controllers/HealthCheckController.kt
+++ src/main/kotlin/tech/challenge/fastfood/fastfood/adapters/controllers/HealthCheckController.kt
@@ -3,11 +3,20 @@ package tech.challenge.fastfood.fastfood.adapters.controllers
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @RestController
-@RequestMapping(name = "health-check")
+@RequestMapping("health-check")
 class HealthCheckController {
 
-    @GetMapping("health")
-    fun healthCheck() = "Alive"
+    @GetMapping
+    fun healthCheck(): String {
+        //TODO fazer health check com enum no futuro
+        val message = "Healthy 🥗"
+
+        val htmlTemplate = String(Files.readAllBytes(Paths.get("src/main/resources/templates/healthCheck.html")))
+
+        return htmlTemplate.replace("{{message}}", message)
+    }
 }
