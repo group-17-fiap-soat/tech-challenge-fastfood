82ff3fd30a66c0eb620a2a955b680097955e5232
Fernando M Santos <meunome950@gmail.com>
2025-03-18 23:16:46 -0300

    feature: ListOrderUseCase ordered

---
+++ src/main/kotlin/tech/challenge/fastfood/fastfood/usecases/order/ListOrderUseCase.kt
@@ -0,0 +1,22 @@
+package tech.challenge.fastfood.fastfood.usecases.order
+
+import org.springframework.stereotype.Service
+import tech.challenge.fastfood.fastfood.adapters.presenters.OrderMapper
+import tech.challenge.fastfood.fastfood.common.daos.OrderDAO
+import tech.challenge.fastfood.fastfood.common.dtos.OrderDto
+import tech.challenge.fastfood.fastfood.common.enums.OrderStatusEnum
+import tech.challenge.fastfood.fastfood.common.interfaces.gateway.OrderGatewayInterface
+
+@Service
+class ListOrderUseCase(
+    private val orderGatewayInterface: OrderGatewayInterface
+) {
+
+    fun execute(): List<OrderDto> {
+        val orders = orderGatewayInterface.findAll()
+            .filter { it.status != OrderStatusEnum.FINISHED }
+            .sortedWith(compareBy<OrderDAO> { it.status?.priority }
+                .thenBy { it.createdAt })
+        return orders.map(OrderMapper::toDto)
+    }
+}
