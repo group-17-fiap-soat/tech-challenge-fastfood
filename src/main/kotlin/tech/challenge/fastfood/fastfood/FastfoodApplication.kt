ec39ce1fb567af5a95485c278cc3ad2f353eb9ff
Manocchio <guruu.leste@gmail.com>
2025-03-22 21:33:52 -0300

    fix

--- src/main/kotlin/tech/challenge/fastfood/fastfood/FastfoodApplication.kt
+++ src/main/kotlin/tech/challenge/fastfood/fastfood/FastfoodApplication.kt
@@ -1,12 +1,16 @@
 package tech.challenge.fastfood.fastfood
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
-
+@OpenAPIDefinition(
+    servers = [Server(url = "/", description = "Default Server URL")]
+)
 @SpringBootApplication
 @EnableJpaAuditing
 @EntityScan
